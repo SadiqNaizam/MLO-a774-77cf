ab6aaad0224d8a57470a4b0e8f0323d12fdcd30a
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-10 10:10:14 +0530

    Files generated: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
 	darkMode: ["class"],
@@ -52,22 +53,19 @@ export default {
 					DEFAULT: 'hsl(var(--card))',
 					foreground: 'hsl(var(--card-foreground))'
 				},
-				sidebar: {
-					DEFAULT: 'hsl(var(--sidebar-background))',
-					foreground: 'hsl(var(--sidebar-foreground))',
-					primary: 'hsl(var(--sidebar-primary))',
-					'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-					accent: 'hsl(var(--sidebar-accent))',
-					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-					border: 'hsl(var(--sidebar-border))',
-					ring: 'hsl(var(--sidebar-ring))'
-				}
+                                success: {
+                                        DEFAULT: 'hsl(var(--success))',
+                                        foreground: 'hsl(var(--success-foreground))'
+                                }
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+                        fontFamily: {
+                                sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+                        },
 			keyframes: {
 				'accordion-down': {
 					from: {
